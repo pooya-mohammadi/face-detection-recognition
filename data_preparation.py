2861af2d9ebda09cc5401ee0d8a1fcc359f0bd02
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-06-25 20:07:48 +0430

    Add face-recognition encoding generation

--- data_preparation.py
+++ data_preparation.py
@@ -1,8 +1,10 @@
-from deep_utils import MTCNNTorchFaceDetector
+from deep_utils import MTCNNTorchFaceDetector, VggFace2TorchFaceRecognition
 
 dataset_dir = "dataset/people"
-
 face_detector = MTCNNTorchFaceDetector()
+face_recognizer = VggFace2TorchFaceRecognition()
 
 if __name__ == '__main__':
     face_detector.detect_crop_dir_of_dir(dataset_dir, get_biggest=True)
+    face_recognizer.extract_dir_of_dir(dataset_dir)
+
