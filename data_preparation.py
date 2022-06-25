de5740454bc4240655ea1f39dd12d68b587e36ba
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-06-26 00:09:09 +0430

    Get mean-encoding.pkl

--- data_preparation.py
+++ data_preparation.py
@@ -6,5 +6,5 @@ face_recognizer = VggFace2TorchFaceRecognition()
 
 if __name__ == '__main__':
     face_detector.detect_crop_dir_of_dir(dataset_dir, get_biggest=True)
-    face_recognizer.extract_dir_of_dir(dataset_dir)
+    face_recognizer.extract_dir_of_dir(dataset_dir, get_mean=True)
 
