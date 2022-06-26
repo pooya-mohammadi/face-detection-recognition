51bd2fe5b39db9ed98f0f958300e631e922cf80b
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-06-26 11:38:27 +0430

    Update preprocessing

--- data_preparation.py
+++ data_preparation.py
@@ -1,10 +1,13 @@
-from deep_utils import MTCNNTorchFaceDetector, VggFace2TorchFaceRecognition
+from argparse import ArgumentParser
+from deep_utils import MTCNNTorchFaceDetector, VggFace2TorchFaceRecognition, UltralightTorchFaceDetector
 
-dataset_dir = "dataset/people"
-face_detector = MTCNNTorchFaceDetector()
-face_recognizer = VggFace2TorchFaceRecognition()
+parser = ArgumentParser()
+parser.add_argument("--dataset_dir", default="dataset/people", help="path to the dataset")
+args = parser.parse_args()
+
+face_detector = UltralightTorchFaceDetector()
+face_recognizer = VggFace2TorchFaceRecognition(model="api/app/weights/senet50_ft_weight.pkl")
 
 if __name__ == '__main__':
-    face_detector.detect_crop_dir_of_dir(dataset_dir, get_biggest=True)
-    face_recognizer.extract_dir_of_dir(dataset_dir, get_mean=True)
-
+    face_detector.detect_crop_dir_of_dir(args.dataset_dir, get_biggest=True)
+    face_recognizer.extract_dir_of_dir(args.dataset_dir, get_mean=True)
