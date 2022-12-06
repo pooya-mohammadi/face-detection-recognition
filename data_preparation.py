cf3fa668f3e4ed48517a249308074dc01eece20a
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-12-06 22:09:23 +0330

    Update final changes to dataset

--- data_preparation.py
+++ data_preparation.py
@@ -6,7 +6,7 @@ parser.add_argument("--dataset_dir", default="dataset/people", help="path to the
 args = parser.parse_args()
 
 face_detector = UltralightTorchFaceDetector()
-face_recognizer = VggFace2TorchFaceRecognition(model="api/app/weights/senet50_ft_weight.pkl")
+face_recognizer = VggFace2TorchFaceRecognition()
 
 if __name__ == '__main__':
     face_detector.detect_crop_dir_of_dir(args.dataset_dir, get_biggest=True)
