ec84e83616bf8ca1d23d54a1257c4f2ca3a0d1ea
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-12-07 11:40:13 +0330

    Update final changes to dataset

--- api/app/weights/get_weights.sh
+++
@@ -1,2 +0,0 @@
-# face-recognition weight
-wget https://github.com/pooya-mohammadi/deep_utils/releases/download/0.9.5/senet50_ft_weight.pkl
--- api/client.py
+++ api/client.py
@@ -5,16 +5,16 @@ from deep_utils import img_to_b64, Box, show_destroy_cv2, split_extension
 import os
 
 parser = ArgumentParser()
-parser.add_argument("--end_point", default="http://127.0.0.1:8000/face_recognition")
+parser.add_argument("--endpoint", default="http://127.0.0.1:8000/face_recognition")
 parser.add_argument("--img_address", default='/home/ai/projects/face-detection-recognition/dataset/test/friends.jpg')
 args = parser.parse_args()
 
 
-def infer(end_point, img_address) -> dict:
+def infer(endpoint, img_address) -> dict:
     image = cv2.imread(img_address)[..., ::-1]
     byte_img = img_to_b64(image)
     data = {'image': byte_img}
-    response = requests.post(end_point, json=data, headers={"Content-Type": "application/json"})
+    response = requests.post(endpoint, json=data, headers={"Content-Type": "application/json"})
     response.raise_for_status()
     return response.json()
 
@@ -26,7 +26,7 @@ def _get_file_content(file_path: str, file_content_type: str = 'multipart/form-d
 
 
 if __name__ == "__main__":
-    output = infer(args.end_point, args.img_address)
+    output = infer(args.endpoint, args.img_address)
     img = cv2.imread(args.img_address)
     for name, distance, box in output.values():
         img = Box.put_box_text(img, box, label=f"{name}-{distance}")
