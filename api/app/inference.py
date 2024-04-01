5bf9a6e8a000d1fd020066988908979f873e41fd
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2024-04-01 17:05:49 +0330

    new deep_utils

--- api/app/inference.py
+++ api/app/inference.py
@@ -1,18 +1,27 @@
 import numpy as np
-from deep_utils import Box, load_pickle
+from deep_utils import Box, PickleUtils
 from deep_utils import VggFace2TorchFaceRecognition, UltralightTorchFaceDetector
 from scipy.spatial.distance import cdist
 
 
 class Inference:
-    def __init__(self, face_recognition_path, encoding_path, threshold, unknown="unknown", device='cpu'):
+    def __init__(self, face_recognition_path, encoding_path, threshold,
+                 distance_metric: str,
+                 face_recognition_model_name: str = "senet50",
+                 unknown="unknown", device='cpu'):
         self.face_detector = UltralightTorchFaceDetector(device=device)
-        self.face_recognizer = VggFace2TorchFaceRecognition(model=face_recognition_path, device=device)
-        names_encodings: dict = load_pickle(encoding_path)
+        self.face_recognizer = VggFace2TorchFaceRecognition(model=face_recognition_path,
+                                                            device=device,
+                                                            model_name=face_recognition_model_name
+                                                            # normalizer_name="",
+                                                            # model_name="incep"
+                                                            )
+        names_encodings: dict = PickleUtils.load_pickle(encoding_path)
         self.encodings = np.array(list(names_encodings.values()))
         self.names = list(names_encodings.keys())
         self.unknown = unknown
         self.threshold = threshold
+        self.distance_metric = distance_metric
 
     @staticmethod
     def preprocessing(img) -> np.ndarray:
@@ -20,9 +29,9 @@ class Inference:
             img = np.array(img).astype(np.uint8)
         return img
 
-    def get_most_similar(self, features: np.ndarray, distance='euclidean'):
-        p = cdist(self.encodings, features, metric=distance)
-        closest_indices, closest_distances = np.argmax(p, axis=0), np.max(p, axis=0)
+    def get_most_similar(self, features: np.ndarray):
+        p = cdist(self.encodings, features, metric=self.distance_metric)
+        closest_indices, closest_distances = np.argmin(p, axis=0), np.min(p, axis=0)
         results = [(self.names[index] if distance < self.threshold else self.unknown, distance) for index, distance in
                    zip(closest_indices, closest_distances)]
         return results
@@ -33,7 +42,7 @@ class Inference:
         face_output = self.face_detector.detect_faces(img, is_rgb=True)
         if face_output['boxes'] and len(face_output['boxes'][0]):
             img_parts = Box.get_box_img(img, face_output['boxes'])
-            results = self.face_recognizer.extract_faces(img_parts, is_rgb=True)
+            results = self.face_recognizer.extract_embeddings(img_parts, is_rgb=True)
             names_distances = self.get_most_similar(results.encodings)
             for e, ((name, distance), box) in enumerate(zip(names_distances, face_output['boxes'])):
                 faces[f"face_{e:02}"] = (name, round(distance, 3), box)
--- api/app/settings.py
+++ api/app/settings.py
@@ -9,13 +9,18 @@ APP_NAME = os.getenv("APP_NAME", 'face_recognition')
 APP_ROOT = os.getenv('APP_ROOT', '/face_recognition')
 HOST = os.getenv("HOST", "127.0.0.1")
 PORT_NUMBER = int(os.getenv('PORT_NUMBER', 8000))
-FACE_RECOGNITION_PATH = os.getenv('FACE_RECOGNITION_PATH', 'weights/senet50_ft_weight.pkl')
+FACE_RECOGNITION_PATH = os.getenv('FACE_RECOGNITION_PATH', "weights/senet50_ft_weight.pkl")
+FACE_RECOGNITION_MODEL_NAME = os.getenv('senet50', "senet50")
 ENCODINGS_PATH = os.getenv("ENCODINGS_PATH", "weights/people.pkl")
 THRESHOLD = float(os.getenv("THRESHOLD", "0.0175"))
 POST_TYPE = os.getenv('POST_TYPE', "JSON")
 DEVICE = os.getenv("DEVICE", 'cpu')
+DISTANCE_METRIC = os.getenv("DISTANCE_METRIC", "euclidean")
 # load the model
-inference = Inference(FACE_RECOGNITION_PATH, ENCODINGS_PATH, threshold=THRESHOLD, device=DEVICE)
+inference = Inference(FACE_RECOGNITION_PATH, ENCODINGS_PATH,
+                      threshold=THRESHOLD, device=DEVICE,
+                      face_recognition_model_name=FACE_RECOGNITION_MODEL_NAME,
+                      distance_metric=DISTANCE_METRIC)
 
 app = Flask(APP_NAME)
 api = Api(app)
