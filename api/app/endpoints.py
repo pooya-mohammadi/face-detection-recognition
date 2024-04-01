db0899050d7da726c878079a0ebe58279f249cc3
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2024-04-01 17:04:48 +0330

    new deep_utils

--- api/app/endpoints.py
+++ api/app/endpoints.py
@@ -1,7 +1,7 @@
 from flask import jsonify
 from flask_restful import Resource
 from settings import app
-from deep_utils import b64_to_img
+from deep_utils import BinaryUtils
 from PIL import Image
 import numpy as np
 
@@ -12,7 +12,7 @@ class FaceDetectionRecognition(Resource):
         args = app.config['PARSER'].parse_args()
         contents = args['image']
         if app.config['POST_TYPE'] == 'JSON':
-            img = b64_to_img(contents)
+            img = BinaryUtils.b64_to_img(contents)
         else:
             img = np.array(Image.open(contents))
         res = app.config['inference'].infer(img)
