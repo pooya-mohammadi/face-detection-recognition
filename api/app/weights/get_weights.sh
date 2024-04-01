df57bf8a18e60f3ea06340450f0b9016be24de4d
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2024-04-01 17:04:23 +0330

    Update get_weights.sh

--- api/app/weights/get_weights.sh
+++ api/app/weights/get_weights.sh
@@ -1,1 +1,3 @@
 wget https://github.com/pooya-mohammadi/deep_utils/releases/download/0.9.5/senet50_ft_weight.pkl
+# wget https://github.com/timesler/facenet-pytorch/releases/download/v2.2.9/20180402-114759-vggface2.pt
+
